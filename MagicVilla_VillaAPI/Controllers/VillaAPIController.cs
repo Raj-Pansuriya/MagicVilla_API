2fa40021ed823b4d5e234c794e85d029aa6f7a3b
Raj Pansuriya <Raj.Pansuriya@mindbodyonline.com>
2023-03-21 13:44:15 +0530

    feat: implement HTTP DELETE request to delete a villa

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -63,5 +63,24 @@ namespace MagicVilla_VillaAPI.Controllers
             //3 -> the created object
             return CreatedAtRoute(routeName:"GetVilla", new {id = villaDTO.Id}, value:villaDTO);
         }
+
+        [HttpDelete("{id:int}", Name = "DeleteVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult DeleteVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            VillaStore.villaList.Remove(villa);
+            return NoContent();
+        }
     }
 }
