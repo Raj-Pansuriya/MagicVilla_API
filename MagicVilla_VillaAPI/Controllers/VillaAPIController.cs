7e56512f3842f1ee61d43baea2d187a3fa1bbc10
Raj Pansuriya <Raj.Pansuriya@mindbodyonline.com>
2023-03-21 09:56:04 +0530

    feat: add an HTTP GET endpoint

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
     [ApiController]
+    [Route("api/VillaAPI")]
     public class VillaAPIController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa {Id=1, Name="Pool View"},
+                new Villa {Id=2, Name="Beach View"}
+            };
+        }
     }
 }
