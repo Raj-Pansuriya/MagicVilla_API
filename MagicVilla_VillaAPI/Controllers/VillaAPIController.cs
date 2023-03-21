15d0cf44885bd41d276aea27cbffe97a6681ccd2
Raj Pansuriya <Raj.Pansuriya@mindbodyonline.com>
2023-03-21 13:59:40 +0530

    feat: implement HTTP PUT request to update the villa object wholly

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -82,5 +82,31 @@ namespace MagicVilla_VillaAPI.Controllers
             VillaStore.villaList.Remove(villa);
             return NoContent();
         }
+
+        [HttpPut("{id:int}", Name = "UpdateVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult UpdateVilla(int id, [FromBody]VillaDTO villaDTO)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            if (villaDTO==null || id != villaDTO.Id)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            villa.Name = villaDTO.Name;
+            villa.Occupancy = villaDTO.Occupancy;
+            villa.Sqft= villaDTO.Sqft;
+
+            return NoContent();
+        }
     }
 }
